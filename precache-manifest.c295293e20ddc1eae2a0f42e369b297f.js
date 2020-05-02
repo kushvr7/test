ac56956e37e1f99c232afbe786c87ad71b8d8c70
kush singh <kushv2660@gmail.com>
2020-05-02 15:28:32 +0530

    Updates

---
+++ precache-manifest.c295293e20ddc1eae2a0f42e369b297f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54126fb5008313e3905958f1dc500041",
+    "url": "/test/index.html"
+  },
+  {
+    "revision": "93e6fc32341242fe61a2",
+    "url": "/test/static/css/main.2fe95c77.chunk.css"
+  },
+  {
+    "revision": "99ea48e159254cc2e91d",
+    "url": "/test/static/js/2.1e5370f4.chunk.js"
+  },
+  {
+    "revision": "5bd09eb7f2cf4d145d532d862be18942",
+    "url": "/test/static/js/2.1e5370f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "93e6fc32341242fe61a2",
+    "url": "/test/static/js/main.a947efaf.chunk.js"
+  },
+  {
+    "revision": "dbe04e375179c869be06",
+    "url": "/test/static/js/runtime-main.fe2037df.js"
+  },
+  {
+    "revision": "d7a6290cb11fc7a1163bc3c7e5405b0a",
+    "url": "/test/static/media/loadeer.d7a6290c.gif"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "d01fdb5b76b50ca72162d6501e9f645c",
+    "url": "/test/static/media/mai.d01fdb5b.jpg"
+  }
+]);
