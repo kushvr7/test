c32a219a8948e6963ad3402e94ffbd05e93a4f05
kush singh <kushv2660@gmail.com>
2020-05-02 14:45:54 +0530

    first commit

---
+++ src/Use/Bod.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+function Bod() {
+
+    const style = {
+        backgroundColor : 'grey',
+        color : 'white',
+        textAlign : 'center',
+        // marginTop : '0px',
+        // height: '40px'
+        // width : '50%',
+        height : 60 ,
+    }
+
+    return (
+        <div style={style} >
+            <h1> Weather Report </h1>
+        </div>
+    )
+}
+
+export default Bod;
